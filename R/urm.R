a8f95b899da8a3e3d5c8b59178ef15a9d3dc25c7
Ben Williams <ben.williams@noaa.gov>
2026-04-09 10:42:32 -0800

    init

---
+++ R/urm.R
@@ -0,0 +1,378 @@
+# unified rockfish model 
+# statistical catch-at-age model generally based on the goa pop model, but works for dusky and northerns as well
+# setup for a single area, single sex, single fleet, single survey 
+# currently has fishery selectivity blocks available
+# ben.williams@noaa.gov
+# 2026-04
+
+# sqrt(1/ (catch_wt * 2))
+
+cmb <- function(f, d) function(p) f(p, d)
+
+
+urm <- function(pars, data) {
+  # load ----
+  require(RTMB)
+  "c" <- RTMB::ADoverload("c")
+  "[<-" <- RTMB::ADoverload("[<-")
+   
+  RTMB::getAll(pars, data)
+ 
+  # setup ----
+  # transform log parameters to natural scale
+  M = exp(log_M)            # natural mortality
+  q = exp(log_q)            # survey catchability
+  slx_pars = exp(log_slx_pars) # selectivity
+  F50 = exp(log_F50)
+  F40 = exp(log_F40)
+  F35 = exp(log_F35)
+ 
+  # bias adjustment if using random effects, yes: bias_switch = 1	
+  bias_adj = bias_switch * ((sigmaR^2) / 2)
+
+   # spawning adjustments
+   spawn_fract = (spawn_mo - 1) / 12           # fraction of year before spawning
+ 
+   # dimensions ----
+   A = nrow(age_error)                         # ages in model
+   A1 = length(ages)                           # ages in comps
+   T = length(years)                           # fishery years
+   Ts = sum(srv_ind)                           # survey years
+   Tfa = sum(fish_age_ind)                     # fishery age comp years
+   Tsa = sum(srv_age_ind)                      # survey age comp years
+   Tfs = sum(fish_size_ind)                    # fishery size comp years
+   L = length(length_bins)                     # lengths in comps
+   n_curves = nrow(slx_pars) 									 # number of selectivity curves
+   g = 0.00001                                 # small number to avoid division by zero
+   
+   # containers ----
+   Bat = Cat = Nat = Fat = Zat = Sat = slx_fish = matrix(0, A, T)   # biomass, catch, numbers, F, Z, S, selectivity at age
+   catch_pred = spawn_bio = tot_bio = rep(0, T) 
+   srv_pred = srv_var = rep(0, Ts)          
+   fish_age_pred = matrix(0, A1, Tfa)
+   srv_age_pred = matrix(0, A1, Tsa)
+   fish_size_pred = matrix(0, L, Tfs)
+   slx_curves = matrix(0, A, n_curves)
+ 
+   # priors ----
+   nll_M = -dnorm(log(M), log(mean_M), cv_M, log = TRUE)
+   nll_q= -dnorm(log(q), log(mean_q), cv_q, log = TRUE)
+   nll_sigmaR= -dnorm(log(sigmaR / mean_sigmaR), 0, cv_sigmaR, log = TRUE)
+ 
+  wt_mature = waa * maa *0.5
+
+   # selectivity ----
+   for(s in 1:n_curves) {
+     slx_curves[,s] = get_slx(ages=1:A, type=slx_type[s], pars=slx_pars[s,], adj=0)
+   }
+   
+   # assign to the fishery over time
+   for(t in 1:T) {
+     slx_fish[,t] = slx_curves[,fish_block_ind[t]]
+   }
+ 
+   # survey selectivity
+   slx_srv = slx_curves[,srv_slx_ind]
+ 
+   # mortality ----
+   Ft = exp(log_mean_F + log_Ft)  # annual fishing mortality on natural scale
+   for(t in 1:T){
+     Fat[,t] = Ft[t] * slx_fish[,t]  # fishing mortality at age and year
+     Zat[,t] = Fat[,t] + M           # total mortality at age and year
+   }
+   Sat = exp(-Zat)                   # survivorship at age and year
+ 
+   # numbers-at-age ----
+   for(t in 1:T) {
+     Nat[1,t] = exp(log_mean_R - bias_adj + log_Rt[t])  # recruitment in year t
+   }
+   # column 1: initial numbers-at-age for each cohort
+   for(a in 2:(A-1)) {
+     Nat[a,1] = exp(log_mean_R - bias_adj - (a-1) * M + init_log_Rt[a-1])
+   }
+   # specific deviation for initial plus group
+   Nat[A,1] = (exp(log_mean_R - bias_adj - (A-1) * M) / (1 - exp(-M))) * exp(init_log_Rt[A-1])      
+ 
+   # forward
+ for(t in 2:T) {
+     for(a in 2:A) {
+       Nat[a,t] = Nat[a-1,t-1] * Sat[a-1,t-1]       # survivors from previous age and year
+     }
+     Nat[A,t] = Nat[A,t] + Nat[A,t-1] * Sat[A,t-1]   # plus group 
+   }
+   recruits = Nat[1,]
+   spawn_bio = colSums(Nat * wt_mature)
+   tot_bio = colSums(Nat * waa)
+ 
+   # mortality adjustment for last year spawning adjustment
+   spawn_adj = Sat[,T]^spawn_fract
+   spawn_bio[T] = sum(Nat[,T] * spawn_adj * wt_mature)
+ 
+   # catch ----
+   Cat = Fat / Zat * Nat * (1-Sat)
+   catch_pred = colSums(Cat * waa)
+   sigma_catch = sqrt(log(catch_cv^2 + 1.0))
+   like_catch = -sum(dnorm(log(catch_obs + g), log(catch_pred + g), sigma_catch, log = TRUE)) 
+ 
+   # survey biomass ----
+   # w/log-normal bias correction
+   isrv = 1
+   srv_like = 0.0
+   
+   for(t in 1:T) {
+     if(srv_ind[t]==1) {
+       srv_pred[isrv] = sum(Nat[,t] * slx_srv * waa) * q 
+       log_sd = sqrt(log(1 + srv_cv[isrv]^2))
+       mu = log(srv_pred[isrv] + g) - 0.5 * log_sd^2
+       srv_like = srv_like -dnorm(log(srv_obs[isrv]), mu, log_sd, log = TRUE)
+       isrv = isrv + 1
+     }
+   }
+   like_srv = srv_like * srv_wt
+ 
+  # fishery age comp ----
+   fish_age_lk = 0.0
+   icomp = 1
+   
+   for(t in 1:T) {
+     if(fish_age_ind[t] == 1) {
+       # predicted age composition (with ageing error)
+       fish_age_pred[,icomp] = as.vector(colSums((Cat[,t] / (sum(Cat[,t]) + g)) * age_error))
+       obs_count = fish_age_obs[,icomp] * fish_age_iss[icomp]  
+       fish_age_lk = fish_age_lk - dmultinom(x = obs_count,
+        																			prob = fish_age_pred[,icomp] + g,
+     																					log = TRUE)
+       icomp = icomp + 1
+     }
+   }
+   like_fish_age = fish_age_lk * fish_age_wt # weighted fishery age comp likelihood
+ 
+ 
+  # survey age comp ----
+   srv_age_lk = 0.0
+   icomp = 1
+   
+   for(t in 1:T) {
+     if(srv_age_ind[t] == 1) {
+       # predicted age composition (with ageing error)
+       srv_age_pred[,icomp] = as.vector(colSums((Nat[,t] * slx_srv) / (sum(Nat[,t] * slx_srv) + g) * age_error))
+       obs_count = srv_age_obs[,icomp] * srv_age_iss[icomp] 
+       srv_age_lk = srv_age_lk - dmultinom(x = obs_count,
+        																		prob = srv_age_pred[,icomp] + g,
+     																				log = TRUE)
+       icomp = icomp + 1
+     }
+   }
+   like_srv_age = srv_age_lk * srv_age_wt # weighted survey age comp likelihood
+ 
+  # fishery size comp ----
+   fish_size_lk = 0.0
+   icomp = 1
+   
+   for(t in 1:T) {
+     if(fish_size_ind[t] == 1) {
+       fish_size_pred[,icomp] = as.vector(colSums((Cat[,t] / (sum(Cat[,t]) + g)) * saa_array[,,fish_saa_ind[t]]))
+       obs_count = fish_size_obs[,icomp] * fish_size_iss[icomp] 
+       fish_size_lk = fish_size_lk - dmultinom(x = obs_count,
+        																			prob = fish_size_pred[,icomp] + g,
+     																					log = TRUE)
+       icomp = icomp + 1
+     }
+   }
+   like_fish_size = fish_size_lk * fish_size_wt # weighted fishery size comp likelihood
+ 
+   # SPR ----
+   Nspr_0 = rep(1,A)
+   for(a in 2:A) {
+     Nspr_0[a] = Nspr_0[a-1] * exp(-M)
+   }
+   Nspr_0[A] = Nspr_0[A] / (1 - exp(-M))
+   SB0 = sum(Nspr_0 * wt_mature * exp(-spawn_fract * M)) # unfished spawning biomass per recruit
+ 
+   # biomass reference points
+   N_50 = N_40 = N_35 = rep(1, A)
+   for(a in 2:A) {
+     N_50[a] = N_50[a-1] * exp(-(M + F50 * slx_fish[a-1,T]))
+     N_40[a] = N_40[a-1] * exp(-(M + F40 * slx_fish[a-1,T]))
+     N_35[a] = N_35[a-1] * exp(-(M + F35 * slx_fish[a-1,T]))
+   }
+   N_50[A] = N_50[A] / (1 - exp(-(M + F50 * slx_fish[A,T])))
+   N_40[A] = N_40[A] / (1 - exp(-(M + F40 * slx_fish[A,T])))
+   N_35[A] = N_35[A] / (1 - exp(-(M + F35 * slx_fish[A,T])))
+ 
+   sbpr_50 = sum(N_50 * wt_mature * exp(-spawn_fract * (M + F50 * slx_fish[,T])))
+   sbpr_40 = sum(N_40 * wt_mature * exp(-spawn_fract * (M + F40 * slx_fish[,T])))
+   sbpr_35 = sum(N_35 * wt_mature * exp(-spawn_fract * (M + F35 * slx_fish[,T])))
+ 
+   sprpen = 1000.0 * ((sbpr_50/SB0 - 0.50)^2 + (sbpr_40/SB0 - 0.40)^2 + (sbpr_35/SB0 - 0.35)^2)
+ 
+   yrs_rec = years[years>=(1977 + ages[1]) & years<=(max(years)-ages[1])]
+   n_rec = length(yrs_rec)
+   pred_rec = mean(Nat[1, years %in% yrs_rec])
+   stdev_rec = sqrt(sum((log_Rt[years %in% yrs_rec] - mean(log_Rt[years %in% yrs_rec]))^2) / (n_rec - 1))
+ 
+   B0 = SB0 * pred_rec
+   B50 = B0 * 0.50
+   B40 = B0 * 0.40
+   B35 = B0 * 0.35
+ 
+   # likelihood/penalties ----
+   like_rec = -sum(dnorm(x = c(log_Rt, init_log_Rt), mean = 0, sd = sigmaR, log = TRUE)) * wt_rec_var
+   f_regularity = -sum(dnorm(x = log_Ft, mean = 0, sd = sigmaF, log = TRUE)) * wt_fmort_reg
+ 
+   # joint negative log-likelihood
+   nll = like_catch + 
+     like_srv + 
+     like_fish_age + 
+     like_srv_age + 
+     like_fish_size + 
+     like_rec + 
+     f_regularity + 
+     nll_M + 
+     nll_q + 
+     nll_sigmaR +
+     sprpen
+   
+   # reports -------------------
+   RTMB::REPORT(ages)
+   RTMB::REPORT(years)
+   RTMB::REPORT(M)
+   RTMB::ADREPORT(M)
+   RTMB::REPORT(slx_pars)
+   RTMB::REPORT(q)
+   RTMB::ADREPORT(q)
+   RTMB::REPORT(sigmaR)
+   RTMB::REPORT(sigmaF)
+   RTMB::REPORT(log_mean_R)
+   RTMB::REPORT(log_Rt)
+   RTMB::ADREPORT(log_Rt)
+   RTMB::REPORT(log_mean_F)
+   RTMB::REPORT(log_Ft)
+   RTMB::REPORT(waa)
+   RTMB::REPORT(maa)
+   RTMB::REPORT(wt_mature)
+   RTMB::REPORT(yield_ratio)
+   RTMB::REPORT(Fat)
+   RTMB::REPORT(Zat)
+   RTMB::REPORT(Sat)
+   RTMB::REPORT(Cat)
+   RTMB::REPORT(Nat)
+   RTMB::REPORT(slx_srv)
+   RTMB::REPORT(slx_fish)
+   RTMB::REPORT(slx_curves)
+   RTMB::REPORT(Ft)
+   RTMB::REPORT(catch_pred)
+   RTMB::REPORT(srv_pred)
+   
+   RTMB::REPORT(fish_age_pred)
+   RTMB::REPORT(srv_age_pred)
+   RTMB::REPORT(fish_size_pred)
+   
+   RTMB::REPORT(tot_bio)
+   RTMB::REPORT(spawn_bio)
+   RTMB::REPORT(recruits)
+   RTMB::ADREPORT(srv_pred)
+   RTMB::ADREPORT(tot_bio)
+   RTMB::ADREPORT(spawn_bio)
+   RTMB::ADREPORT(recruits)
+   RTMB::REPORT(spawn_fract)
+   RTMB::REPORT(B0)
+   RTMB::REPORT(B40)
+   RTMB::REPORT(B35)
+   RTMB::REPORT(F35)
+   RTMB::REPORT(F40)
+   RTMB::REPORT(F50)
+   
+   # note: ADREPORT reference points to get uncertainty estimates (delta method)
+   RTMB::ADREPORT(B0)
+   RTMB::ADREPORT(B50)
+   RTMB::ADREPORT(B40)
+   RTMB::ADREPORT(B35)
+   RTMB::ADREPORT(F35)
+   RTMB::ADREPORT(F40)
+   RTMB::ADREPORT(F50)
+   
+   RTMB::REPORT(pred_rec)
+   RTMB::REPORT(n_rec)
+   RTMB::REPORT(yrs_rec)
+   RTMB::REPORT(stdev_rec)
+   
+   RTMB::REPORT(like_catch)
+   RTMB::REPORT(like_srv)
+   RTMB::REPORT(like_fish_age)
+   RTMB::REPORT(like_srv_age)
+   RTMB::REPORT(like_fish_size)
+   RTMB::REPORT(like_rec)
+   RTMB::REPORT(f_regularity)
+   RTMB::REPORT(nll_q)
+   RTMB::REPORT(nll_M)
+   RTMB::REPORT(nll_sigmaR)
+   RTMB::REPORT(nll)
+   
+   return(nll)
+ 
+ 
+}
+
+pars1 = list(log_M = log(0.0614),
+             log_slx_pars = log_slx_pars,
+             log_q = log(1.15),
+             log_mean_R = 3.0,
+             init_log_Rt = rep(0, 27),
+             log_Rt = rep(0, length(data1$years)),
+             log_mean_F = 0,
+             log_Ft = rep(0, length(data1$years)),
+             sigmaR = 1.7,
+             sigmaF = 1.0,
+             log_F50 = log(0.05), 
+             log_F40 = log(0.06), 
+             log_F35 = log(0.07))
+
+data = list(ages = ,        						# vector - rec_age to plus_age
+  					years = ,										# vector - all years of catch 
+            length_bins = , 						# vector 
+            spawn_mo = ,    						# scalar - when they spawn
+            bias_switch = 0, 						# scalar (1/0) on for random effects, default: 0
+            waa = ,          						# vector - weight at age (grams)
+            maa = ,											# vector - maturity at age 
+            slx_type = ,								# vector - length: number of selectivity curves, minimum need 2 (fishery and survey) 
+            														# 1=logistic (2 pars), 2=gamma (2 pars), 3=double normal (4 pars), 4=double logistic (4 pars)
+            fish_block_ind = , 					# vector - length (years), integer mapping each year to a selectivity curve 
+            srv_slx_ind = , 						# integer - pointer to which curve in log_slx_pars belongs to the survey
+            catch_obs = ,  							# vector - catch by year, (tons)
+            catch_cv = ,   							# vector - catch cv = 0.1 to match the historical weighting of 50
+            srv_ind = ,									# vector - survey index, dim: length(years) 1 = use, 0 = ignore
+            srv_yrs = ,									# vector - survey years
+            srv_obs = ,									# vector - survey obs (unit: tons)
+            srv_cv = ,									# vector - survey cv
+            srv_wt = 1.0,								# scalar - survey weight
+            fish_age_ind = ,						# vector - fish age comp index, dim: length(years) 1 = use, 0 = ignore
+            fish_age_yrs = ,						# vector - fish age comp years
+            fish_age_obs = ,						# matrix - fishery age comp observations, dim: ages, length(fish_age_yrs)
+            fish_age_iss = ,						# vector - fish age comp input sample size, dim: length(fish_age_yrs)
+            fish_age_wt = 1.0,					# scalar - fish age comp weight
+            srv_age_ind = ,							# vector - survey age comp index, dim length(years) 1 = use, 0 = ignore
+            srv_age_yrs = ,							# vector - survey age comp years
+            srv_age_obs = ,							# matrix - survey age comp observations, dim: ages, length(srv_age_yrs)
+            srv_age_iss = ,							# vector - survey age comp input sample size, dim: length(srv_age_yrs)
+            srv_age_wt = 1.0,						# scalar - survey age comp weight
+            fish_size_ind = ,						# vector - fish size comp index, dim: length(years) 1 = use, 0 = ignore
+            fish_size_yrs = ,						# vector - fish size comp years
+            fish_size_obs = ,						# matrix - fish proportions at length, dim: A, length(fish_size_yrs))
+            fish_size_iss = ,						# vector - fish size comp input sample size, dim: length(fish_size_yrs)
+            fish_size_wt = 1.0,					# scalar - fish size comp weight
+            age_error = ,								# matrix - age error (can be a rectangular matrix)
+            saa_array = ,								# array  - of size-at-age matrixes (dim: ages, length_bins)
+            fish_saa_ind =, 						# vector - which saa_array to use, (dim: length(years)))
+            wt_fmort_reg = 0.1,					# fishing mortality regulation weight
+            wt_rec_var = 1,							# recruitment variability weight
+            mean_M = 0.0614,						# natural mortality prior
+            cv_M = 0.1,									# natural mortality cv
+            mean_q = 1.15, 							# catchbility prior
+            cv_q = 0.447213595,					# catchability cv
+            mean_sigmaR = 1.7, 					# sigmaR prior
+            cv_sigmaR = 0.2,  					# sigmaR cv
+            yield_ratio = yield_ratio) 	# scalar - yield ratio to pass through to projection module
+m25aa = run_model(urm, data1, pars1, map = list(log_slx_pars = map_slx_factor))
+
+
